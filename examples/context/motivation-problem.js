c91ee4b46eec88e10d17b8ee0d85e2e49339069e
QiChang Li <github@liqichang.com>
2019-02-22 17:08:49 +0800

    Update examples/context/motivation-problem.js
    Co-Authored-By: BetaMee <gongxq95@gmail.com>

--- examples/context/motivation-problem.js
+++ examples/context/motivation-problem.js
@@ -5,7 +5,7 @@ class App extends React.Component {
 }
 
 function Toolbar(props) {
-  // highlight-range{1-3,6}
+  // highlight-range{1-4,7}
   // Toolbar 组件接受一个额外的“theme”属性，然后传递给 ThemedButton 组件。
   // 如果应用中每一个单独的按钮都需要知道 theme 的值，这会是件很麻烦的事，
   // 因为必须将这个值层层传递所有组件。
