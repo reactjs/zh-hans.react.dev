fb346be6b1e82a517673db6fced7343016014222
QiChang Li <github@liqichang.com>
2019-02-22 17:09:00 +0800

    Update examples/context/motivation-solution.js
    Co-Authored-By: BetaMee <gongxq95@gmail.com>

--- examples/context/motivation-solution.js
+++ examples/context/motivation-solution.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     // highlight-range{1-3,5}
     // 使用一个 Provider 来将当前的 theme 传递给以下的组件树。
     // 无论多深，任何组件都能读取这个值。
-    // 在这个例子中，我们将“dark”作为当前的值传递下去。
+    // 在这个例子中，我们将 “dark” 作为当前的值传递下去。
     return (
       <ThemeContext.Provider value="dark">
         <Toolbar />
