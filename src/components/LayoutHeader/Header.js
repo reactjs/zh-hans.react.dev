78ebbba8e34b2551f88c4f6ddf3886b4c59ac77b
QC-L <github@liqichang.com>
2019-02-25 16:12:32 +0800

    docs(en): fix conflicts

--- src/components/LayoutHeader/Header.js
+++ src/components/LayoutHeader/Header.js
@@ -131,21 +131,13 @@ const Header = ({location}: {location: Location}) => (
             to="/tutorial/tutorial.html"
           />
           <HeaderLink
-<<<<<<< HEAD
-            isActive={location.pathname.includes('/community/')}
-            title="社区"
-            to="/community/support.html"
-          />
-          <HeaderLink
-=======
->>>>>>> bab2a3491a18e7074799272d60c8a159473bb7f5
             isActive={location.pathname.includes('/blog')}
             title="博客"
             to="/blog/"
           />
           <HeaderLink
             isActive={location.pathname.includes('/community/')}
-            title="Community"
+            title="社区"
             to="/community/support.html"
           />
         </nav>
