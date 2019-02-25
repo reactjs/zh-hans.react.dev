955cb4f654f3c8b9c3cb3eae4eaf2cdf86f77889
QC-L <github@liqichang.com>
2019-02-25 16:12:32 +0800

    docs(en): fix conflicts

--- src/components/LayoutHeader/DocSearch.js
+++ src/components/LayoutHeader/DocSearch.js
@@ -97,13 +97,8 @@ class DocSearch extends Component<{}, State> {
           }}
           id="algolia-doc-search"
           type="search"
-<<<<<<< HEAD
-          placeholder="在文档中搜索..."
+          placeholder="搜索"
           aria-label="在文档中搜索..."
-=======
-          placeholder="Search"
-          aria-label="Search docs"
->>>>>>> bab2a3491a18e7074799272d60c8a159473bb7f5
         />
       </form>
     ) : null;
