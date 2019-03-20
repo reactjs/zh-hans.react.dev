d493ede17d6587e34ff75d49ae42f059ee83bb0a
QiChang Li <github@liqichang.com>
2019-03-20 18:06:50 +0800

    Update customized-display-name.js

--- examples/forwarding-refs/customized-display-name.js
+++ examples/forwarding-refs/customized-display-name.js
@@ -8,7 +8,7 @@ function logProps(Component) {
   }
 
   // 在 DevTools 中为该组件提供一个更有用的显示名。
-  // 例如“ForwardRef(logProps(MyComponent))”
+  // 例如 “ForwardRef(logProps(MyComponent))”
   // highlight-range{1-2}
   const name = Component.displayName || Component.name;
   forwardRef.displayName = `logProps(${name})`;
