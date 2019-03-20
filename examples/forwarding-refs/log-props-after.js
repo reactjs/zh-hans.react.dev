9554f6e1e5eb399a97c83744b9330c03a76ef527
QiChang Li <github@liqichang.com>
2019-03-20 18:06:21 +0800

    Update log-props-after.js

--- examples/forwarding-refs/log-props-after.js
+++ examples/forwarding-refs/log-props-after.js
@@ -9,13 +9,13 @@ function logProps(Component) {
       // highlight-next-line
       const {forwardedRef, ...rest} = this.props;
 
-      // 将自定义的 prop 属性“forwardedRef”定义为 ref
+      // 将自定义的 prop 属性 “forwardedRef” 定义为 ref
       // highlight-next-line
       return <Component ref={forwardedRef} {...rest} />;
     }
   }
 
-  // 注意 React.forwardRef 回调的第二个参数“ref”。
+  // 注意 React.forwardRef 回调的第二个参数 “ref”。
   // 我们可以将其作为常规 prop 属性传递给 LogProps，例如 “forwardedRef”
   // 然后它就可以被挂载到被 LogPros 包裹的子组件上。
   // highlight-range{1-3}
