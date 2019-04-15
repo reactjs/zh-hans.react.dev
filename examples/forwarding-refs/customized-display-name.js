ea0be446d331c7ce5e16535cd567c08f6d3c7318
Yang Hang <yhsunshining@gmail.com>
2019-04-15 17:37:16 +0800

    docs(cn): translate content/docs/forwarding-refs.md into Chinese (#118)

--- examples/forwarding-refs/customized-display-name.js
+++ examples/forwarding-refs/customized-display-name.js
@@ -7,8 +7,8 @@ function logProps(Component) {
     return <LogProps {...props} forwardedRef={ref} />;
   }
 
-  // Give this component a more helpful display name in DevTools.
-  // e.g. "ForwardRef(logProps(MyComponent))"
+  // 在 DevTools 中为该组件提供一个更有用的显示名。
+  // 例如 “ForwardRef(logProps(MyComponent))”
   // highlight-range{1-2}
   const name = Component.displayName || Component.name;
   forwardRef.displayName = `logProps(${name})`;
