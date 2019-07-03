2284f6c7d5e4e4acdf79cb845f0d035a3dd23fae
QC-L <github@liqichang.com>
2019-07-03 16:01:19 +0800

    fix(ci): modified config.yml

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,4 +3,5 @@ git config --global user.email "github@liqichang.com"
 git remote set-url origin git@github.com:reactjs/zh-hans.reactjs.org.git
 
 chmod -R 777 node_modules/gh-pages/
+yarn build
 yarn deploy
