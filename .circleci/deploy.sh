4e90087589727e0d0f5d42a424a3a29acd028ebb
QC-L <github@liqichang.com>
2019-07-03 15:44:23 +0800

    feat: add circle ci

---
+++ .circleci/deploy.sh
@@ -0,0 +1,6 @@
+git config --global user.name "QC-L"
+git config --global user.email "github@liqichang.com"
+git remote set-url origin git@github.com:reactjs/zh-hans.reactjs.org.git
+
+chmod -R 777 node_modules/gh-pages/
+yarn deploy
