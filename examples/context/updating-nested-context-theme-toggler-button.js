b34c0e8a5d4ea1a14274a13561a23a3d24fb7c2e
QiChang Li <github@liqichang.com>
2021-03-17 17:51:01 +0800

    Update updating-nested-context-theme-toggler-button.js

--- examples/context/updating-nested-context-theme-toggler-button.js
+++ examples/context/updating-nested-context-theme-toggler-button.js
@@ -2,14 +2,14 @@ import {ThemeContext} from './theme-context';
 
 function ThemeTogglerButton() {
   // highlight-range{1-2,5}
-  // Theme Toggler 按钮不仅仅只获取 theme 值，它也从 context 中获取到一个 toggleTheme 函数
+  // Theme Toggler 按钮不仅仅只获取 theme 值，
+  // 它也从 context 中获取到一个 toggleTheme 函数
   return (
     <ThemeContext.Consumer>
       {({theme, toggleTheme}) => (
         <button
           onClick={toggleTheme}
-          style={{backgroundColor: theme.background}}
-        >
+          style={{backgroundColor: theme.background}}>
           Toggle Theme
         </button>
       )}
