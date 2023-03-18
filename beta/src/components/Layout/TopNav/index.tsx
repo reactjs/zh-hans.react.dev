88adae0a29f8b35d44535f867fc78dd437f0bf8b
Dan Abramov <dan.abramov@gmail.com>
2023-03-18 20:39:52 +0000

    Remove the new website code from the legacy repo

--- beta/src/components/Layout/TopNav/index.tsx
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright (c) Facebook, Inc. and its affiliates.
- */
-
-export {default as TopNav} from './TopNav';
--- beta/src/utils/rafShim.js
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright (c) Facebook, Inc. and its affiliates.
- */
-
-// Used in next.config.js to remove the raf transitive dependency.
-export default window.requestAnimationFrame;
