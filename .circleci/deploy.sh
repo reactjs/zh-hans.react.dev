bc069f862e55edda568d5fed23913638c4dfc3a9
QC-L <github@liqichang.com>
2023-03-20 14:15:17 +0800

    fix: remove redundant file content

--- .circleci/deploy.sh
+++
@@ -1,7 +0,0 @@
-git config --global user.name "QC-L"
-git config --global user.email "github@liqichang.com"
-git remote set-url origin git@github.com:reactjs/zh-hans.reactjs.org.git
-
-chmod -R 777 node_modules/gh-pages/
-yarn build
-yarn deploy
