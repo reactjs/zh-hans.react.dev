58ae0ed323e43747a5070022404e11318496d220
Xavi Lee <awxiaoxian2020@163.com>
2023-03-29 13:22:53 +0800

    update index title

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ class Home extends Component {
     return (
       <Layout location={location}>
         <TitleAndMetaTags
-          title="React 官方中文文档 &ndash; 用于构建用户界面的 JavaScript 库"
+          title="已过时——React 中文文档"
           canonicalUrl={createCanonicalUrl('/')}
         />
         <div
